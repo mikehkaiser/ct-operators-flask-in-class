f3a659bd51706d463a6ec6d1840442ac1c8ad4bd
mikehkaiser <mikehkaiser@gmail.com>
2021-08-02 16:03:17 -0500

    day-1

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__)
+
+# Gives access to the project in ANY OS we find ourselves in
+# Allows outside files/folders to be added to the project from the base directory)
---
+++ drone_inventory/site/routes.py
@@ -0,0 +1,11 @@
+# This is the controller page for drone_inventory
+
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+
+@site.route('/') #the '/' is the route for any homepage
+
+def home():
+    return render_template('index.html')
